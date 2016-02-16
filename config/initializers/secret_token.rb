aba3bdd21fe1aef14905f995a7d0f1b3077ba6c4
heldermmelo <heldermmelo@gmail.com>
2016-02-16 16:06:21 -0300

    programa vazio/primeiro commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PrimeiroApp::Application.config.secret_token = 'c680c31f77fd7d43aa2524cc6f25280ea3676f236008aa4fca540368f7e92ba7a53289ab7a9fde7b00b3eea1d35b80426827d7fe8d8a80c1fb6ee9cf74219914'
